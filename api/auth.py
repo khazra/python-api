8e1d89855fbbb88a3251bd41b4cde0c7e135f4ea
Michal Kloczko <michal.kloczko@stxnext.pl>
2016-05-16 15:38:11 +0200

    experiment with authenticating

--- api/auth.py
+++ api/auth.py
@@ -1,10 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint, request, Response
+from functools import wraps
 auth = Blueprint('auth', __name__)
 
 
+def check_auth(authorization_token):
+    return authorization_token == 'token'
+
+
+def authenticate():
+    return Response('Not authorized', 401)
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        authorization_token = request.headers.get('Token')
+        if not check_auth(authorization_token):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
 @auth.route('/login', methods=['POST'])
+@requires_auth
 def login():
-    return 'OK'
+    return Response('OK', 200)
 
 
 @auth.route('/logout', methods=['POST'])
--- app.py
+++ app.py
@@ -4,4 +4,5 @@ app = Flask(__name__)
 
 
 app.register_blueprint(auth)
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
