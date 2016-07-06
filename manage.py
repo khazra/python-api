6d9f4ea8c1fab324ff7d3916ed4fd3d083fe1902
Michal Kloczko <michal.kloczko@stxnext.pl>
2016-07-06 18:01:39 +0200

    add and configure coverage

--- manage.py
+++ manage.py
@@ -1,4 +1,7 @@
 import unittest
+import coverage
+import os
+
 from app import app
 
 from flask_script import Manager
@@ -9,10 +12,46 @@ manager = Manager(app)
 @manager.command
 def test():
     """Runs the unit tests without coverage."""
+
     tests = unittest.TestLoader().discover('tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
+
     if result.wasSuccessful():
         return 0
+
+    return 1
+
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    cov = coverage.Coverage(
+        branch=True,
+        cover_pylib=False,
+        data_file='.coverage',
+        include=[
+            'tests/*',
+            'api/*'
+        ],
+        omit='.venv/*'
+    )
+    cov.start()
+
+    tests = unittest.TestLoader().discover('tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if result.wasSuccessful():
+        cov.stop()
+        cov.save()
+        print('\n\nCoverage Summary:\n')
+        cov.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, '.tmp/coverage')
+        cov.html_report(directory=covdir)
+        print('\n\nHTML version: file://%s/index.html\n' % covdir)
+        cov.erase()
+        return 0
+
     return 1
 
 if __name__ == '__main__':
--- tests/test__config.py
+++ tests/test__config.py
@@ -8,7 +8,6 @@ from flask_testing import TestCase
 
 
 class TestDevelopmentConfig(TestCase):
-
     def create_app(self):
         app.config.from_object('config.Development')
         return app
@@ -16,6 +15,17 @@ class TestDevelopmentConfig(TestCase):
     def test_app_is_development(self):
         self.assertTrue(app.config['DEBUG'] is True)
         self.assertTrue(app.config['LOG_LEVEL'] is logging.INFO)
+        self.assertFalse(current_app is None)
+
+
+class TestProductionConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('config.Production')
+        return app
+
+    def test_app_is_production(self):
+        self.assertTrue(app.config['DEBUG'] is False)
+        self.assertTrue(app.config['LOG_LEVEL'] is logging.ERROR)
         self.assertFalse(current_app is None)
 
 
